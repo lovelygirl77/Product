eef2a03abfd0b3581b0d4a4dd70a7c1bd878c368
幻想 <aysun@huanxiangdepro.lan>
2016-10-25 13:45:32 +0800

    新建两个ViewController

---
+++ CreatDemo/CreatDemo/AddViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AddViewController.swift
+//  CreatDemo
+//
+//  Created by 幻想 on 16/10/25.
+//  Copyright © 2016年 zqq. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ CreatDemo/CreatDemo/bViewController.swift
@@ -0,0 +1,22 @@
+//
+//  bViewController.swift
+//  CreatDemo
+//
+//  Created by 幻想 on 16/10/25.
+//  Copyright © 2016年 zqq. All rights reserved.
+//
+
+import UIKit
+
+class bViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("新建的第二个文件")
+    }
+
+
+
+}
